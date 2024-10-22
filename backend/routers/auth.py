3f0e28437f78e7d5f1cf9e379a655ec7223d249c
Juan Bedoya <juanchischis2010@hotmail.com>
2024-10-22 15:33:07 -0500

    se creo un nuevo metodo para obtener el nombre del usuario por email y se cambio el string que se devolvia en algunos errores

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from schemas.user_schema import User, UserLogin
 from services.auth import AuthService
 
+
 auth_router = APIRouter()
 
 @auth_router.post("/register", tags=["auth"])
@@ -18,6 +19,7 @@ def register(user: User):
 def login(user:UserLogin):
     try:
         AuthService().login_user(user)
+
         return JSONResponse(status_code=200, content={"success": True, "Token": "Token" , "message" : "login sucessfull"})
     except ValueError:
-        return JSONResponse(status_code=400, content={"sucess": False , "message": "Email or password incorrect" })
+        return JSONResponse(status_code=400, content={"sucess": False , "message": "Email or password" })
