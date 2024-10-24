6810c3895f77ae1ddded63e3fd98cf1ec3e651f6
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-10-24 16:25:46 -0600

    agrega ruta /login

---
+++ client/app/routes/login.tsx
@@ -0,0 +1,47 @@
+import { Form } from "@remix-run/react";
+import Button from "~/components/Button";
+import { ButtonState } from "~/utilities/enums";
+
+export default function Login () {
+    return (
+        <div className="min-h-dvh center-full">
+            <main className="min-h-dvh center-full w-90-auto max-w-[400px] md:border-[1px] md:border-gray-low md:px-[40px] md:max-w-[480px] md:rounded-2xl md:min-h-[700px]">
+                <div className="text-4xl font-bold mb-6 flex space-x-3">
+                <h1>Instaflare</h1> 
+                </div>
+                <p className="text-lg w-[180px] text-center mb-12"><span className="text-sky-pearl">Comparte</span> tu mundo y <span className="text-sky-blue">descubre</span> otros.</p>
+
+                <Form
+                    className="w-full space-y-10 mb-6"
+                >
+                    <label  className="flex flex-col space-y-3">
+                        <span className="text-sm font-medium">Correo electrónico</span>
+                        <input
+                            className="text-sm bg-black111 border-[1px] border-gray-low rounded-full h-[40px] px-6 outline-none transition-colors focus:border-white-full"
+                            aria-label="Correo electrónico"
+                            name="email"
+                            type="text" 
+                        />
+                    </label>
+
+                    <label className="flex flex-col space-y-3">
+                        <span className="text-sm font-medium">Contraseña</span>
+                        <input
+                            className="text-sm bg-black111 border-[1px] border-gray-low rounded-full h-[40px] px-6 outline-none transition-colors focus:border-white-full"
+                            aria-label="contraseña"
+                            name="pass"
+                            type="text" 
+                        />
+                    </label>
+
+                    <Button
+                        state={ButtonState.Active}
+                    >
+                        Iniciar sesion
+                    </Button>
+                </Form>
+                <p>¿No tienes cuenta? <a href="" className="font-medium transition-colors text-sky-base hover:text-sky-hover active:text-sky-active">Registrate</a></p>
+            </main>
+        </div>
+    );
+}
