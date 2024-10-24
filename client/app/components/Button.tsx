7b755f8c3c44ab38bf1638294456204fd59dc387
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-10-24 16:23:18 -0600

    agrega componente Button

---
+++ client/app/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ButtonState } from "~/utilities/enums";
+
+export default function Button({ children, state }: { children: string, state: ButtonState }) {
+    const ActiveStyles = "text-sky-base shadow-glow-sm shadow-sky-base/20 hover:bg-gray-hover hover:shadow-glow-lg hover:shadow-sky-base/20 active:text-sky-active active:bg-gray-active"
+    const InactiveStyles = "cursor-default text-white-low"
+    const buttonState = state === ButtonState.Active ? ActiveStyles : InactiveStyles;
+    return (
+        <button
+            className={`bg-gray-base px-6 h-[40px] text-center font-bold w-full rounded-full transition-colors ${buttonState}`}
+            type="submit"
+        >{children}</button>
+    );
+}
