122dc9b9c5f7abd619f94e91783c911b406a2912
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-11-04 22:23:24 -0600

    limita width en pantallas grandes

--- client/app/routes/_layout.tsx
+++ client/app/routes/_layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "~/components/Navbar";
 
 export default function Layout() {
     return(
-        <main className="grid grid-rows-layout min-h-dvh">
+        <main className="grid grid-rows-layout min-h-dvh center-full mx-auto w-full max-w-[400px] md:border-[1px] md:border-gray-low md:px-[40px] md:max-w-[480px] md:rounded-2xl md:min-h-[700px]">
             <Outlet />
             <div className="h-[75px]"></div> // Espacio abajo
             <Navbar />
